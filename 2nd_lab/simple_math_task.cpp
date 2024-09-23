c70f3a1c1cfee128644fef7ff69a466ea46e8377
Daniel <129957112+dakovisuals@users.noreply.github.com>
2024-09-23 15:20:04 +0200

    Update simple_math_task.cpp
    Signed-off-by: Daniel <129957112+dakovisuals@users.noreply.github.com>

--- 2nd_lab/simple_math_task.cpp
+++ 2nd_lab/simple_math_task.cpp
@@ -3,19 +3,24 @@
 
 int main()
 {
+
+    std::cout << "Variant #7." << std::endl;
+
     int A;
     int B;
     int C;
-    std::cout << "Input A side ( all values should be in (0 ; 3*10^4) ): ";
+    
+    std::cout << "*All values should be in (0 ; 3*10^4)" << std::endl;
+    std::cout << "Input A side: ";
     std::cin >> A; 
     std::cout << "Input B side: ";
     std::cin >> B;
     std::cout << "Input C side: ";
     std::cin >> C;
 
-    int diag = 0;
-    diag = pow(A, 2) + pow(B, 2) + pow(C, 2);
-    std::cout << "The diagonal of a rectangular parallelepiped is: " << diag << std::endl;
+    float diag = 0;
+    diag = pow(pow(A, 2) + pow(B, 2) + pow(C, 2), 0.5);
+    std::cout << "The diagonal of a rectangular parallelepiped is " << diag << std::endl;
 
     return 0;
 
